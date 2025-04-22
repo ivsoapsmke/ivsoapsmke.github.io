7aff4e16a50b345edf7dbbe6d2cd924bf05281dd
basedcookie <lickcalls@gmail.com>
2025-04-22 16:51:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const productsUrl = "https://checkout.ivsoapsmke.com/checkout.php?products=all";
 const productElement = document.getElementsByClassName('soapBar')[0];
+const productContainer = document.getElementById('soaps');
 var clonedProduct;
 
 async function fetchAsync (productsUrl) {
@@ -8,16 +9,26 @@ async function fetchAsync (productsUrl) {
     return data;
 }
 
-async function getProducts() {
+async function getProducts(e) {
     const productsData = await fetchAsync(productsUrl);
     for (const product in productsData) {
-        clonedProduct = productElement.cloneNode(true);
+        clonedProduct = e.cloneNode(true);
         createProduct(productsData[product], clonedProduct);
     }
+    productElement.style.display = "none";
 }
 
 function createProduct(p, c) {
     console.log(p);
+    c.querySelector('.soap-name').innerHTML = p.name;
+    c.querySelector('.soap-price').innerHTML = "$"+ p.price +" / bar";
+    c.querySelector('.soap-desc').innerHTML = p.description;
+    for(let x = 0; x < p.images.length; x++){
+        c.querySelector('.soap-pic').src = p.images[x];
+        c.querySelector('.soap-pic').alt = p.name + " Bar Picture;
+    }
+    productContainer.appendChild(c);
+    console.log(c);
 }
 
-getProducts();
+getProducts(productElement);
