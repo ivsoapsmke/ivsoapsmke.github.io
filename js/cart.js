a8db05421132d6c9361e812879fd9fb0d77b019c
basedcookie <lickcalls@gmail.com>
2025-05-09 17:53:13 -0500

    Cart Updates

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,24 @@
 var cartContents = {};
 
 function editCart(price_id, qty, type){
-    console.log(price_id, qty);
+    if (price_id in cartContents) {
+        cartContents[price_id] = cartContents[price_id]+qty;
+    } else {
+        cartContents[price_id] = qty;
+    }
+    console.log(cartContents);
+}
+
+function checkoutCart(cart){
+    let cartArray = [];
+    for (var price_id in cart) {
+        if (cart.hasOwnProperty(price_id)) {
+            let productArray = [];
+            productArray["price"] = price_id;
+            productArray["quantity"] = cart[price_id];
+            cartArray.push(productArray);
+        }
+    }
+
+    console.log(cartArray);
 }
